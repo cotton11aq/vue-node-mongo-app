b5598455e227ad4998b0a07219f36c2113c55aa8
WataruTamura <wataru.tamura@rakus-partners.co.jp>
2021-07-31 20:50:59 +0900

    front

--- server/client/src/api/index.js
+++ server/client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default () => {
+export const Api = () => {
   return axios.create({
     baseURL: `http://localhost:3000/`,
   });
--- server/client/src/views/Home.vue
+++ server/client/src/views/Home.vue
@@ -1,7 +1,36 @@
-<template lang="pug"></template>
+<template lang="pug">
+div
+  h1 ホーム
+  ul(v-for='(todo, index) in todos')
+    li(key=index) {{ todo.title }}
+      input(type='checkbox', :checked='todo.done')
+      router-link(to='/') 編集
+      button 削除
+</template>
 
 <script>
+import Methods from '@/api/methods';
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      todos: '',
+    };
+  },
+  created() {
+    this.getTodos();
+  },
+  methods: {
+    async getTodos() {
+      try {
+        const todos = await Methods.getTodos();
+        this.todos = todos.data.todos;
+        console.log(this.todos);
+      } catch (err) {
+        console.log(err.message);
+      }
+    },
+  },
 };
 </script>
